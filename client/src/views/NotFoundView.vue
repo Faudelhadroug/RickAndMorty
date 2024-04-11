0b10ba439cf4e8593841131dff72abbf6cef6868
Faudel <hadroug.faudel@gmail.com>
2024-04-11 18:05:00 +0200

    style: center text and font style

--- client/src/views/NotFoundView.vue
+++ client/src/views/NotFoundView.vue
@@ -3,8 +3,10 @@
 </script>
 
 <template>
-  <main>
-    <h1>Error 404: Page not found</h1>
-    <a href="/"><p>Go back at home</p></a>
+  <main class="h-screen w-full flex flex-col justify-center items-center">
+    <h1 class="text-bold text-2xl xl:text-5xl pb-[1rem]">
+      Error 404: Page not found
+    </h1>
+    <a class="text-xl xl:text-3xl underline" href="/"><p>Go back at home</p></a>
   </main>
 </template>
